6bc930e34e5828299bbdf1e0d32dc066d0d5747b
MefitHp <mefitdev@gmail.com>
2019-05-29 16:20:12 -0500

    Queried posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,44 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { graphql, useStaticQuery } from "gatsby"
 const blog = () => {
+  let {
+    allMarkdownRemark: { edges },
+  } = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
-      <div>This is the blog</div>
+      <h1>This is the blog</h1>
+      <ol>
+        {edges.map((post, index) => {
+          let {
+            node: {
+              frontmatter: { title, date },
+            },
+          } = post
+
+          return (
+            <li key={index}>
+              {/* el div */}
+              <h2>{title}</h2>
+              <p>{date}</p>
+            </li>
+          )
+        })}
+      </ol>
     </Layout>
   )
 }
